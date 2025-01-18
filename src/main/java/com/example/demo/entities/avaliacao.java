ec1910a24a0c66357b201c9aa1a016984830663e
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-01-17 21:25:33 -0300

    Update avaliacao.java

--- src/main/java/com/example/demo/entities/avaliacao.java
+++ src/main/java/com/example/demo/entities/avaliacao.java
@@ -1,4 +1,29 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class avaliacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private Date data;
+
+    private double nota;
+
+    private String comentarios;
+
 }
