5271987d19f201342d7544347a42a43016694182
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-01-20 12:54:21 -0300

    Add TelaInicialController

--- src/main/java/com/example/demo/controllers/TelaInicialController.java
+++ src/main/java/com/example/demo/controllers/TelaInicialController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.telas.aluno.TelaAlunosInicial;
 import com.example.demo.telas.exemplo.TelaAlunos;
 import javafx.fxml.FXML;
 import org.springframework.stereotype.Controller;
@@ -7,5 +8,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TelaInicialController {
 
+    private TelaAlunosInicial telaAlunosInicial;
+
+    public TelaInicialController(TelaAlunosInicial telaalunosInicial) {
+        this.telaAlunosInicial = telaAlunosInicial;
+    }
+
+    @FXML
+    protected void onAlunosButtonClick() {
+        telaAlunosInicial.abrir();
+    }
 
 }
