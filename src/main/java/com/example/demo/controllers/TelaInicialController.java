86105b5354a1dd0583a9a27280c9f15272108ea9
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2025-02-11 20:11:43 -0300

    .

--- src/main/java/com/example/demo/controllers/TelaInicialController.java
+++ src/main/java/com/example/demo/controllers/TelaInicialController.java
@@ -7,5 +7,40 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TelaInicialController {
 
+    private TelaAlunos telaAlunos;
+    private TelaAlunos telaAdicionarAluno;
+    private TelaAlunos telaAtualizarAluno;
+    private TelaAlunos telaVisualizarAluno;
+
+    public TelaInicialController(TelaAlunos telaAlunos, TelaAlunos telaAdicionarAluno,
+                                 TelaAlunos telaAtualizarAluno, TelaAlunos telaVisualizarAluno) {
+        this.telaAlunos = telaAlunos;
+        this.telaAdicionarAluno = telaAdicionarAluno;
+        this.telaAtualizarAluno = telaAtualizarAluno;
+        this.telaVisualizarAluno = telaVisualizarAluno;
+
+    }
+
+    @FXML
+    protected void onAbrirAlunosButtonClick() {
+        telaAlunos.abrir();
+    }
+
+    @FXML
+    protected void onNovoAlunoButtonClick(){
+        telaAdicionarAluno.abrir();
+    }
+
+    @FXML
+    protected void onExcluirAlunoButtonClick(){
+        telaAtualizarAluno.abrir();
+    }
+
+    @FXML
+    protected void onEditarAlunoButtonClick(){
+        telaVisualizarAluno.abrir();
+    }
+
+
 
 }
